b524ef348c1df44789bd12e8255ecb62f2ba77f4
mbax <matt@phozop.net>
2014-08-01 20:40:17 -0400

    Consistent formatting, except Updater

--- src/com/volcanicplaza/minetrends/ResponseProtocol.java
+++ src/com/volcanicplaza/minetrends/ResponseProtocol.java
@@ -9,26 +9,23 @@ import org.codehaus.jackson.map.annotate.JsonSerialize;
 
 @JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
 @Generated("com.googlecode.jsonschema2pojo")
-@JsonPropertyOrder({
-"RESPONSE_CODE",
-"RESPONSE_MESSAGE"
-})
+@JsonPropertyOrder({"RESPONSE_CODE", "RESPONSE_MESSAGE"})
 public class ResponseProtocol {
-	
+
 	@JsonProperty("RESPONSE_CODE")
 	private int RESPONSE_CODE;
-	
+
 	@JsonProperty("RESPONSE_MESSAGE")
 	private String RESPONSE_MESSAGE;
-	
+
 	@JsonProperty("RESPONSE_CODE")
 	public int getRESPONSE_CODE() {
-	return RESPONSE_CODE;
+		return RESPONSE_CODE;
 	}
-	
+
 	@JsonProperty("RESPONSE_MESSAGE")
 	public String getRESPONSE_MESSAGE() {
-	return RESPONSE_MESSAGE;
+		return RESPONSE_MESSAGE;
 	}
 
 }
--- src/com/volcanicplaza/minetrends/sendRunnable.java
+++ src/com/volcanicplaza/minetrends/sendRunnable.java
@@ -21,17 +21,17 @@ public class sendRunnable implements Runnable {
 		String data = null;
 		long startTime = 0;
 		HttpURLConnection conn = null;
-		
+
 		//System.out.println(Minetrends.getData());
-		
-		 String urlParameters = "key=" + Minetrends.publicKey + "&data=" + Minetrends.getData();
-		  try {
-			  url = new URL(Minetrends.hostname + ":81");
-			  
-		  } catch (Exception ex){
-			  ex.printStackTrace();
-		  }
-		
+
+		String urlParameters = "key=" + Minetrends.publicKey + "&data=" + Minetrends.getData();
+		try {
+			url = new URL(Minetrends.hostname + ":81");
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
 		try {
 			conn = (HttpURLConnection) url.openConnection();
 			conn.setRequestMethod("POST"); //use post method
@@ -42,26 +42,26 @@ public class sendRunnable implements Runnable {
 			conn.setInstanceFollowRedirects(true);
 			conn.setAllowUserInteraction(false);
 			conn.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
-			conn.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-		
+			conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
 			// Open a stream which can write to the URL******************************************
 			DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
 			wr.writeBytes(urlParameters);
 			wr.flush();
 			wr.close();
-			
+
 			startTime = System.currentTimeMillis();
 			// Open a stream which can read the server response*********************************
 			InputStream in = conn.getInputStream();
-			BufferedReader rd  = new BufferedReader(new InputStreamReader(in));
+			BufferedReader rd = new BufferedReader(new InputStreamReader(in));
 			String responseSingle;
 			while ((responseSingle = rd.readLine()) != null) {
-			String response = null;
-			response = response + responseSingle;
+				String response = null;
+				response = response + responseSingle;
 			}
 			String response = rd.readLine();
-			
-			if (response != null){
+
+			if (response != null) {
 				ObjectMapper mapper = new ObjectMapper();
 				ResponseProtocol responseObj = mapper.readValue(response, ResponseProtocol.class);
 				if (responseObj.getRESPONSE_CODE() != 200) {
@@ -69,7 +69,7 @@ public class sendRunnable implements Runnable {
 					Minetrends.plugin.getLogger().info("Error: <" + responseObj.getRESPONSE_CODE() + "> " + responseObj.getRESPONSE_MESSAGE());
 				}
 			}
-			
+
 			rd.close(); //close the reader
 			//System.out.println("Response: " + response);
 			if (in != null) {
@@ -82,13 +82,13 @@ public class sendRunnable implements Runnable {
 			e.printStackTrace();
 		} finally { //in this case, we are ensured to close the connection itself
 			if (conn != null)
-			conn.disconnect();
+				conn.disconnect();
 		}
 		long endTime = System.currentTimeMillis();
 		long duration = endTime - startTime;
 		//System.out.println("Took " + duration + " ms.");
 		//Done communicating with server
-		}
+	}
 
 
 }
