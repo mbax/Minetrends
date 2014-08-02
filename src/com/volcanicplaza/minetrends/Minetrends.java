4230663d675800bcddcc2ce7e780d8f1925cb9fc
mbax <matt@phozop.net>
2014-08-01 20:46:31 -0400

    remove IRRESPONSIBLE code (See below)
    All tasks for a plugin are automatically disabled on plugin disable.
    Calling cancelAllTasks cancels ALL plugins' tasks. EVERY PLUGIN.

--- src/com/volcanicplaza/minetrends/Minetrends.java
+++ src/com/volcanicplaza/minetrends/Minetrends.java
@@ -93,7 +93,6 @@ public class Minetrends extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		Bukkit.getLogger().info(getDescription().getName() + " v" + getDescription().getVersion() + " has been disabled!");
-		Bukkit.getScheduler().cancelAllTasks();
 	}
 
 	public static int getFrequency() {
